8643870dae91be87440742233e80daa6122d916a
Dhanuka Sheyam Perera <dhanuka.sp@outlook.com>
2024-04-13 08:23:27 +0530

    basic component structure for expenses added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+import Expenses from './components/Expenses';
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insuarance",
+      amount: 240.25,
+      date: new Date(2024, 4, 23)
+    },
+    {
+      id: "e2",
+      title: "Food",
+      amount: 15.75,
+      date: new Date(2024, 4, 1)
+    },
+    {
+      id: "e3",
+      title: "Tea",
+      amount: 5.12,
+      date: new Date(2024, 4, 4)
+    },
+    {
+      id: "e4",
+      title: "Electricity Bill",
+      amount: 300.51,
+      date: new Date(2024, 4, 30)
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <div>
+      <h2>Let's get started...</h2>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import './ExpenseDate.css';
+
+function ExpenseDate(props){
+    const month = props.dateObject.toLocaleString('en-US', {month: 'long'});
+    const day = props.dateObject.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.dateObject.getFullYear();
+
+    return (
+        <div className="expense-date">
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__day">{day}</div>
+            <div className="expense-date__year">{year}</div>
+        </div>
+    );
+}
+
+export default ExpenseDate;
